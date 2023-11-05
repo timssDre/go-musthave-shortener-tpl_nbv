f70d9dacc1e7a7fedb80041cd917b8838bdf03aa
Timur <111347232+timssDre@users.noreply.github.com>
2023-11-05 16:01:31 +0600

    iter3_2

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -26,9 +27,14 @@ func Test_shortenURLHandler(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			r := gin.Default()
+			r.POST("/", shortenURLHandler)
+
 			request := httptest.NewRequest(http.MethodPost, "/", nil)
 			w := httptest.NewRecorder()
-			shortenURLHandler(w, request)
+
+			r.ServeHTTP(w, request)
+
 			res := w.Result()
 			defer res.Body.Close()
 			assert.Equal(t, tt.args.code, res.StatusCode)
@@ -59,9 +65,12 @@ func Test_redirectToOriginalURLHandler(t *testing.T) {
 
 	for _, tt := range testsGET {
 		t.Run(tt.name, func(t *testing.T) {
+			r := gin.Default()
+			r.GET("/:id", redirectToOriginalURLHandler)
+
 			request := httptest.NewRequest(http.MethodGet, "/asd", nil)
 			w := httptest.NewRecorder()
-			redirectToOriginalURLHandler(w, request)
+			r.ServeHTTP(w, request)
 			res := w.Result()
 			defer res.Body.Close()
 			assert.Equal(t, tt.argsGet.code, res.StatusCode)
