41f9dd5bd7caf391fa4a973af4e407acdf30077d
Timur <111347232+timssDre@users.noreply.github.com>
2023-11-11 21:41:31 +0600

    iter5_17

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
+	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/api"
 	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/config"
-	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/storage"
 )
 
 func main() {
 	addrConfig := config.InitConfig()
-
-	app := struct {
-		api *storage.Storage
-	}{}
-
-	app.api = storage.New(addrConfig.BaseURL)
-
-	err := app.api.Start(addrConfig.ServerAddr)
-	if err != nil {
-		panic(err)
-	}
+	api.Start(addrConfig.ServerAddr, addrConfig.BaseURL)
 }
--- internal/api/handler.go
+++ internal/api/handler.go
@@ -3,14 +3,14 @@ package api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/serviceUsage"
+	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/utility"
 	"io"
 
 	"net/http"
 	"strings"
 )
 
-func (s *Storage) ShortenURLHandler(c *gin.Context) {
+func (s *Screwdriver) ShortenURLHandler(c *gin.Context) {
 	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Failed to read request body", http.StatusInternalServerError)
@@ -18,8 +18,8 @@ func (s *Storage) ShortenURLHandler(c *gin.Context) {
 	}
 	URLtoBody := strings.TrimSpace(string(body))
 
-	shortID := serviceUsage.RandSeq(8)
-	s.urlMap[shortID] = URLtoBody
+	shortID := utility.RandSeq(8)
+	s.UrlMap[shortID] = URLtoBody
 
 	shortURL := fmt.Sprintf("%s/%s", s.BaseURL, shortID)
 
@@ -27,9 +27,9 @@ func (s *Storage) ShortenURLHandler(c *gin.Context) {
 	c.String(http.StatusCreated, shortURL)
 }
 
-func (s *Storage) RedirectToOriginalURLHandler(c *gin.Context) {
+func (s *Screwdriver) RedirectToOriginalURLHandler(c *gin.Context) {
 	shortID := c.Param("id")
-	originalURL, exists := s.urlMap[shortID]
+	originalURL, exists := s.UrlMap[shortID]
 	if exists {
 		c.Header("Location", originalURL)
 		c.String(http.StatusTemporaryRedirect, originalURL)
