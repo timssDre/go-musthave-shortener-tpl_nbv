63552d589ec5df24ff53b26147e165fd7b297ba7
Timur <111347232+timssDre@users.noreply.github.com>
2023-11-17 17:41:06 +0600

    iter6_1

--- go.mod
+++ go.mod
@@ -47,6 +47,8 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.starlark.net v0.0.0-20220816155156-cfacd8902214 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
--- internal/api/api.go
+++ internal/api/api.go
@@ -3,15 +3,22 @@ package api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/logger"
 	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/services"
 	"github.com/timssDre/go-musthave-shortener-tpl_nbv.git/internal/storage"
+	"go.uber.org/zap"
 )
 
 type RestAPI struct {
 	StructService *services.ShortenerService
 }
 
-func StartRestAPI(ServerAddr, BaseURL string, storage *storage.Storage) error {
+func StartRestAPI(ServerAddr, BaseURL string, LogLevel string, storage *storage.Storage) error {
+	if err := logger.Initialize(LogLevel); err != nil {
+		return err
+	}
+	logger.Log.Info("Running server", zap.String("address", ServerAddr))
+
 	storageShortener := services.NewShortenerService(BaseURL, storage)
 	api := &RestAPI{
 		StructService: storageShortener,
@@ -19,6 +26,8 @@ func StartRestAPI(ServerAddr, BaseURL string, storage *storage.Storage) error {
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+
+	r.Use(logger.RequestLogger(logger.Log), gin.Recovery())
 
 	api.setRoutes(r)
 
